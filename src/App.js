0cbe67b98f51785be5d954e6da9097e4a4c311e9
Ekaterina <katrinamalinashtorm@gmail.com>
2021-07-21 01:53:39 +0600

    Init App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Container from "@material-ui/core/Container";
+import AppToolbar from "./components/AppToolbar/AppToolbar";
+import Routes from "./Routes";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return(
+      <>
+        <CssBaseline />
+        <AppToolbar/>
+        <main>
+          <Container>
+              <Routes/>
+          </Container>
+        </main>
+      </>
+  )};
 
 export default App;
